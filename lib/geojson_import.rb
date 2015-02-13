4073e58e46fef0146a9cbcdd2bc0df0f48e91b22
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2015-02-13 16:02:11 -0300

    Requiring all files

--- lib/geojson_import.rb
+++ lib/geojson_import.rb
@@ -1,4 +1,12 @@
-require "geojson_import/version"
+require 'geojson_import/version'
+
+require 'geojson_import/downloader'
+require 'geojson_import/feature'
+require 'geojson_import/feature_collection'
+require 'geojson_import/importer'
+require 'geojson_import/indexer'
+require 'geojson_import/point_checker'
+require 'geojson_import/point_collector'
 
 module Geojson
 
