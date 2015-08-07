4aefec763e8eba37b316530eccba24e7a233dda4
Juan Blandini <jblandini@manas.com.ar>
2015-08-07 17:26:45 -0300

    Fix for instedd/cepheid-web#405 Aleutians West in Alaska location, displayed over United Kingdom Fixing issues with shapes that cross the Longitude 180 or -180

--- lib/geojson_import/point_collector.rb
+++ lib/geojson_import/point_collector.rb
@@ -10,14 +10,27 @@ module Geojson
 
     def <<(point)
       lng, lat = point
+      if not_first_iteration && (lng < @min_lng && (@min_lng - lng) > 180)
+        lng = lng + 360
+      end
+      if not_first_iteration && (lng > @max_lng && (@max_lng - lng) < -180)
+        lng = lng - 360
+      end
       @min_lat = lat if lat < @min_lat
       @max_lat = lat if lat > @max_lat
       @min_lng = lng if lng < @min_lng
       @max_lng = lng if lng > @max_lng
     end
 
+    def not_first_iteration
+      @min_lng != 180 && @max_lng != -180
+    end
+
     def center
-      [(@min_lng + @max_lng) / 2, (@min_lat + @max_lat) / 2]
+      lng = (@min_lng + @max_lng) / 2
+      lng = lng - 360 if lng > 180
+      lng = lng + 360 if lng < -180
+      [lng, (@min_lat + @max_lat) / 2]
     end
 
     def self.compute_center(coords)
